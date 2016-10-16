533b2a236983b3b305562cd030e063653ab53bb3
thebeebs <martinbeeby@hotmail.com>
2016-10-16 21:44:16 +0100

    Adding tests plus fixing performance issue on mobile with responsive menu.

---
+++ PersonalSite/Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsStoryReturingCorrectFriendlyDate()
+        {
+            var s = new Story();
+
+        }
+    }
+}
---
+++ PersonalSite/src/PersonalTests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersonalSite.ViewModels;
+
+namespace SampleNetCoreUnitTests
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void TestMethodPassing()
+        {
+            var s = new Story();
+            
+            Assert.IsTrue(s.Author == null);
+            
+        }
+
+        [TestMethod]
+        public void TestMethodFailing()
+        {
+            var s = new Story();
+            Assert.IsTrue(s.Author == null);
+        }
+    }
+}
